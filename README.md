e0c1c471d83665115b02a9120de3e366b9ef195e
rink1969 <rink1969@gmail.com>
2023-03-07 02:48:00 -0500

    add script and update readme

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # manifest
 Manifests to release CITACloud automated with git repo tool
+
+# usage
+
+## install repo
+```bash
+curl https://mirrors.tuna.tsinghua.edu.cn/git/git-repo -o repo
+chmod +x repo
+sudo mv repo /usr/local/bin/
+# add next line to ~/.bashrc
+export REPO_URL='https://mirrors.tuna.tsinghua.edu.cn/git/git-repo'
+```
+
+## release
+For example, release version is `6.6.5-beta1`.
+
+### repo init
+```bash
+repo init -u git@github.com:cita-cloud/manifest.git -b main -m default.xml
+repo sync
+```
+
+### tag cita_cloud_proto
+Create a tag `6.6.5-beta1` for repo cita_cloud_proto.
+
+### publish cloud-common-rs
+
+Create a branch `v6.6.5-beta1` for repo cloud-common-rs.
+
+Publish creates cloud-util `0.6.2-beta1` and cita_cloud_proto `6.6.5-beta1`.
+
+### release micro services
+
+install deps
+
+```bash
+pip install rtoml
+```
+
+```bash
+repo start v6.6.5-beta1 --all
+export PROTO_VERSION=6.6.5-beta1
+export UTIL_VERSION=0.6.2-beta1
+# copy update-deps.py to current dir
+repo forall -p -v -c python3 `pwd`/update-deps.py
+
+repo forall -p -v -c git add .
+repo forall -p -v -c git commit -m "release v6.6.5-beta1"
+
+repo forall -p -v -c git push origin HEAD
+```
+
+
---
+++ update-deps.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import rtoml
+import os
+from pathlib import Path
+
+def get_env():
+    util_version = os.getenv("UTIL_VERSION")
+    if util_version is None:
+        raise Exception("need set environment variable for UTIL_VERSION")
+    proto_version = os.getenv("PROTO_VERSION")
+    if proto_version is None:
+        raise Exception("need set environment variable for PROTO_VERSION")
+
+    return util_version, proto_version
+
+if __name__ == '__main__':
+    util_version, proto_version = get_env()
+
+    cargo_path = Path('Cargo.toml')
+
+    project = rtoml.load(cargo_path)
+    # print(project)
+
+    try:
+        if project['dependencies']['cloud-util']:
+            project['dependencies']['cloud-util'] = "=" + util_version
+    except (KeyError):
+        pass
+    
+    try:
+        if project['dependencies']['cita_cloud_proto']:
+            project['dependencies']['cita_cloud_proto'] = "=" + proto_version
+    except (KeyError):
+        pass
+    
+    try:
+        if project['dependencies']['crypto_sm']:
+            project['dependencies']['crypto_sm']["branch"] = "v" + proto_version
+    except (KeyError):
+        pass
+
+    try:
+        if project['dependencies']['crypto_eth']:
+            project['dependencies']['crypto_eth']["branch"] = "v" + proto_version
+    except (KeyError):
+        pass
+
+    
+    rtoml.dump(project, cargo_path, pretty=True)
